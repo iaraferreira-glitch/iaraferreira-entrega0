1225f386e0a568f02c8d950e2a5aa4135f9c1a4c
Iara Ferreira <ferreiraiara.10@gmail.com>
2025-08-19 03:00:38 -0300

    Este commit tiene lo siguiente: el desafíate, donde redirigimos a login desde el index cuando se detecta que no se está loggeado. También tiene el guardar datos para que quede loggeado.

---
+++ js/authentication.js
@@ -0,0 +1,7 @@
+//para verificar si hay una sesión activa
+if (localStorage.getItem('loggedIn') !== 'true') {
+    //Si no existe una sesión activa, a continuación redirigimos al usuario a la página de login
+    window.location.href = "login.html";
+}
+
+//Cree este js básicamente para que si la persona no está conectada no le permita acceder a otras zonas al poner en el https el nombre de la zona. Por ejemplo si alguien intenta ir a https://ecommerce.com/zona.html será expulsado si no inicia sesión. 
--- js/login.js
+++ js/login.js
@@ -1,4 +1,10 @@
+// aquí comienza el redirect al home luego de realizar un login exitoso
 function redirectToHome() {
+
+    localStorage.setItem("loggedIn", "true");
+
+    //divido la función en dos partes, una que redirija, la parte principal, pero la primera es la que se asegura que los datos queden guardados.
+
     window.location.href = "index.html";
 }
 
