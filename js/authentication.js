a4b8db98f0372b1e4cc7971dc9de619fd786d276
Iara Ferreira <ferreiraiara.10@gmail.com>
2025-08-19 23:02:52 -0300

    Agregué todo lo que es css al login

--- js/authentication.js
+++ js/authentication.js
@@ -1,7 +1,11 @@
 //para verificar si hay una sesión activa
+
 if (localStorage.getItem('loggedIn') !== 'true') {
-    //Si no existe una sesión activa, a continuación redirigimos al usuario a la página de login
-    window.location.href = "login.html";
+
+//Si no existe una sesión activa, a continuación redirigimos al usuario a la página de login
+
+window.location.href = "login.html";
 }
 
-//Cree este js básicamente para que si la persona no está conectada no le permita acceder a otras zonas al poner en el https el nombre de la zona. Por ejemplo si alguien intenta ir a https://ecommerce.com/zona.html será expulsado si no inicia sesión. 
+//Cree este js básicamente para que si la persona no está conectada no le permita acceder a otras zonas al poner en el https el nombre de la zona. 
+// Por ejemplo si alguien intenta ir a https://ecommerce.com/my-perfil.html será expulsado si no inicia sesión. 
--- js/login.js
+++ js/login.js
@@ -12,3 +12,28 @@ const btnLogin = document.getElementById("btn-login");
 if (btnLogin) {
     btnLogin.addEventListener("click", redirectToHome);
 }
+
+//js para el toggle de password
+
+const togglePassword = document.getElementById("togglePassword");
+const password = document.getElementById("password");
+
+togglePassword.addEventListener("click", function(){
+
+    if (password.type === "password") {
+
+        //esto es para mostrarla si está oculta
+        password.type = "text";
+
+        //y aquí es para cambiar el icono.
+
+        togglePassword.classList.remove("fa-eye");
+        togglePassword.classList.add("fa-eye-slash");
+        
+    } else{
+        password.type = "password";
+
+        togglePassword.classList.remove("fa-eye-slash");
+        togglePassword.classList.add("fa-eye");
+    }
+})
